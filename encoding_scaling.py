4c076778ea572dabf3016f1250d672ca74021f45
Furkan TOSUN <74304973+Scorpowl@users.noreply.github.com>
2023-08-09 15:24:47 +0300

    Update encoding_scaling.py

--- encoding_scaling.py
+++ encoding_scaling.py
@@ -78,6 +78,36 @@ def label_encoder(dataframe,binary_col):
     dataframe[binary_col] = label_encoder.fit_transform(dataframe[binary_col])
     return dataframe
 
+def one_hot_encoder(dataframe, categorical_cols, drop_first = False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols,drop_first=drop_first)
+    return dataframe
+
+def cat_summary(dataframe, col_name, plot = False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("#########################################")
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+def rare_analyser(dataframe, target , cat_cols):
+    for col in cat_cols:
+        print(col,":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                                       "RATIO": dataframe[col].value_counts() / len(dataframe),
+                                       "TARGET MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+def rare_encoder(dataframe, rare_perc):
+    temp_df = dataframe.copy()
+
+    rare_columns = [col for col in temp_df.columns if temp_df[col].dtype == "O" and (temp_df[col].value_counts() / len(temp_df) < rare_perc).any(axis = None)]
+
+    for var in rare_columns:
+        tmp = temp_df[var].value_counts() / len(temp_df)
+        rare_labels = tmp[tmp < rare_perc].index
+        temp_df[var] = np.where(temp_df[var].isin(rare_labels), "Rare", temp_df[var])
+
+    return temp_df
 
 # binary_cols = [col for col in df.columns if df[col].dtype not in [int,float] and df[col].nunique() == 2]
 
@@ -102,38 +132,42 @@ def label_encoder(dataframe,binary_col):
 #     label_encoder(a,col)
 # print(a)
 
-def one_hot_encoder(dataframe, categorical_cols, drop_first = False):
-    dataframe = pd.get_dummies(dataframe, columns=categorical_cols,drop_first=drop_first)
-    return dataframe
-
 # cat_cols, num_cols, cat_but_car = grab_col_names(df)
 # ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
 # print(ohe_cols)
 
-dff = load_application_train()
-dff["NAME_EDUCATION_TYPE"].value_counts()
+# dff = load_application_train()
+# dff["NAME_EDUCATION_TYPE"].value_counts()
 
-cat_cols, num_cols, cat_but_car = grab_col_names(dff)
-
-def cat_summary(dataframe, col_name, plot = False):
-    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
-                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
-    print("#########################################")
-    if plot:
-        sns.countplot(x=dataframe[col_name], data=dataframe)
-        plt.show()
+# cat_cols, num_cols, cat_but_car = grab_col_names(dff)
 
 # for i in cat_cols:
 #     cat_summary(dff,i)
+# rare_analyser(dff, "TARGET", cat_cols)
 
-def rare_analyser(dataframe, target , cat_cols):
-    for col in cat_cols:
-        print(col,":", len(dataframe[col].value_counts()))
-        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
-                                       "RATIO": dataframe[col].value_counts() / len(dataframe),
-                                       "TARGET MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+# new_df  =  rare_encoder(dff,0.01)
+# rare_analyser(new_df, "TARGET", cat_cols)
+# print(dff["OCCUPATION_TYPE"].value_counts)
 
-rare_analyser(dff, "TARGET", cat_cols)
+df = load()
+ss = StandardScaler()
+df["Age_standard_scaler"] = ss.fit_transform(df[["Age"]])
+
+rs = RobustScaler()
+df["Age_robust_scaler"] = rs.fit_transform(df[["Age"]])
+
+mms = MinMaxScaler()
+df["Age_minmax_scaler"] = mms.fit_transform(df[["Age"]])
+
+print(df.describe().T)
+
+
+
+
+
+
+
+
 
 
 
